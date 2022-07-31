122cd97f0a1f0820a73dce6721306608c64765c3
Adam Mendl <amendl@hotmail.com>
2022-07-31 17:44:18 +0200

    hmm

---
+++ hmm.jl
@@ -0,0 +1,24 @@
+# Hidden markov models
+
+
+#
+function markovModel(matrix::Array{T,2},state::Array{T,2}) where T<:AbstractFloat
+
+end
+#
+function hiddenMarkovModel(matrix::Array{T,2},states::Array{T,2}) where T<:AbstractFloat
+
+end
+
+function Viterbi() where T<:AbstractFloat
+
+end
+
+function ForwardBackward() where T<:AbstractFloat
+
+end
+
+function BaumWelch() where T<:AbstractFloat
+
+end
+
